69b786bf37e3b9829058e651a831afefcebce1e7
Nicolas Leroux <hertford.fr@gmail.com>
2025-11-15 15:43:02 +0100

    refactor: rename application

--- src/main/java/eu/nicosworld/stoptaclop/StoplaclopApplication.java
+++ src/main/java/eu/nicosworld/stoptaclop/StoplaclopApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationApplication {
+public class StoplaclopApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AuthenticationApplication.class, args);
+    SpringApplication.run(StoplaclopApplication.class, args);
   }
 }
